69b103c9be12b05e0a1e3b109eddde644d2bb6a2
Wilson Lam <wlamcuhk@gmail.com>
2021-03-27 08:17:46 +0800

    experiment with process and system methods

--- CppND-System-Monitor-Project-Updated/include/process.h
+++ CppND-System-Monitor-Project-Updated/include/process.h
@@ -1,32 +1,3 @@
-// #ifndef PROCESS_H
-// #define PROCESS_H
-//
-// #include <string>
-// /*
-// Basic class for Process representation
-// It contains relevant attributes as shown below
-// */
-// class Process {
-//  public:
-//   Process(const int& pid);
-//   int Pid() const;
-//   std::string User();
-//   std::string Command();
-//   float CpuUtilization() const;
-//   void CpuUtilization(long active_jiffies, long system_jiffies);
-//   std::string Ram();
-//   long int UpTime() const;
-//   bool operator<(Process const& other) const;
-//
-//  private:
-//   int pid_;
-//   float cpu_{0};
-//   long prev_active_jiffies_{0};
-//   long prev_system_jiffies_{0};
-// };
-//
-// #endif
-
 #ifndef PROCESS_H
 #define PROCESS_H
 
@@ -37,24 +8,21 @@ It contains relevant attributes as shown below
 */
 class Process {
  public:
-  Process(int pid);
-  int Pid() const;               // DONE: See src/process.cpp
-  std::string User() const;      // DONE: See src/process.cpp
-  std::string Command() const;   // DONE: See src/process.cpp
-  float CpuUtilization() const;  // DONE: See src/process.cpp
-  void CpuUtilization(long active_ticks, long system_ticks);
-  std::string Ram() const;                 // DONE: See src/process.cpp
-  long int UpTime() const;                 // DONE: See src/process.cpp
-  bool operator<(Process const& a) const;  // DONE: See src/process.cpp
-  bool operator>(Process const& a) const;  // DONE: See src/process.cpp
+  Process(const int& pid);
+  int Pid() const;
+  std::string User() const;
+  std::string Command() const;
+  float CpuUtilization() const;
+  void CpuUtilization(const long& system_jiffies);
+  std::string Ram() const;
+  long int UpTime() const;
+  bool operator>(Process const& other) const;
 
-  // DONE: Declare any necessary private members
  private:
   int pid_;
   float cpu_{0};
-  long cached_active_ticks_{0};
-  long cached_idle_ticks_{0};
-  long cached_system_ticks_{0};
+  long prev_active_jiffies_{0};
+  long prev_system_jiffies_{0};
 };
 
 #endif
--- CppND-System-Monitor-Project-Updated/src/process.cpp
+++ CppND-System-Monitor-Project-Updated/src/process.cpp
@@ -1,51 +1,3 @@
-// #include "process.h"
-//
-// #include <unistd.h>
-//
-// #include <cctype>
-// #include <sstream>
-// #include <string>
-// #include <vector>
-//
-// #include "linux_parser.h"
-//
-// using std::string;
-// using std::to_string;
-// using std::vector;
-//
-// Process::Process(const int& pid) : pid_(pid) {}
-//
-// // Return this process's ID
-// int Process::Pid() const { return pid_; }
-//
-// // Return this process's CPU utilization
-// float Process::CpuUtilization() const { return cpu_; }
-//
-// // Calculate & update CPU usage
-// void Process::CpuUtilization(long active_jiffies, long system_jiffies) {
-//   long active_duration = active_jiffies - prev_active_jiffies_;
-//   long system_duration = system_jiffies - prev_system_jiffies_;
-//   cpu_ = static_cast<float>(active_duration) / system_duration;
-//   prev_active_jiffies_ = active_duration;
-//   prev_system_jiffies_ = system_jiffies;
-// }
-//
-// // Return the command that generated this process
-// string Process::Command() { return LinuxParser::Command(pid_); }
-// // Return this process's memory utilization
-// string Process::Ram() { return LinuxParser::Ram(pid_); }
-//
-// // Return the user (name) that generated this process
-// string Process::User() { return LinuxParser::User(pid_); }
-//
-// // Return the age of this process (in seconds)
-// long int Process::UpTime() const { return LinuxParser::UpTime(pid_); }
-//
-// bool Process::operator<(Process const& other) const {
-//   return CpuUtilization() < other.CpuUtilization();
-// }
-
-
 #include "process.h"
 
 #include <unistd.h>
@@ -61,40 +13,37 @@ using std::string;
 using std::to_string;
 using std::vector;
 
-// Add constructor for Process
-Process::Process(int pid) : pid_(pid) {}
+Process::Process(const int& pid) : pid_(pid) {
+  const long system_jiffies = LinuxParser::Jiffies();
+  CpuUtilization(system_jiffies);
+}
 
-// DONE: Return this process's ID
+// Return this process's ID
 int Process::Pid() const { return pid_; }
 
-// DONE: Return this process's CPU utilization
+// Return this process's CPU utilization
 float Process::CpuUtilization() const { return cpu_; }
 
-void Process::CpuUtilization(long active_ticks, long system_ticks) {
-  long duration_active{active_ticks - cached_active_ticks_};
-  long duration{system_ticks - cached_system_ticks_};
-  cpu_ = static_cast<float>(duration_active) / duration;
-  cached_active_ticks_ = active_ticks;
-  cached_system_ticks_ = system_ticks;
+// Calculate & update CPU usage
+void Process::CpuUtilization(const long& system_jiffies) {
+  const long active_duration = LinuxParser::ActiveJiffies(Pid()) - prev_active_jiffies_;
+  const long system_duration = system_jiffies - prev_system_jiffies_;
+  cpu_ = active_duration * 1.0 / system_duration;
+  prev_active_jiffies_ = active_duration;
+  prev_system_jiffies_ = system_jiffies;
 }
 
-// DONE: Return the command that generated this process
+// Return the command that generated this process
 string Process::Command() const { return LinuxParser::Command(Pid()); }
-
-// DONE: Return this process's memory utilization
+// Return this process's memory utilization
 string Process::Ram() const { return LinuxParser::Ram(Pid()); }
 
-// DONE: Return the user (name) that generated this process
+// Return the user (name) that generated this process
 string Process::User() const { return LinuxParser::User(Pid()); }
 
-// DONE: Return the age of this process (in seconds)
+// Return the age of this process (in seconds)
 long int Process::UpTime() const { return LinuxParser::UpTime(Pid()); }
 
-// DONE: Overload the "less than" comparison operator for Process objects
-bool Process::operator>(const Process& a) const {
-  return CpuUtilization() > a.CpuUtilization();
-}
-
-bool Process::operator<(const Process& a) const {
-  return CpuUtilization() < a.CpuUtilization();
+bool Process::operator>(Process const& other) const {
+  return CpuUtilization() > other.CpuUtilization();
 }
