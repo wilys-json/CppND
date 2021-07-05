12e067c334bde83592757e13f11eee4c49f7c260
Lam_Family <wlamcuhk@gmail.com>
2021-07-06 07:30:23 +0800

    updated Snake movements

--- CppND-Capstone-Snake-Game/src/snake.cpp
+++ CppND-Capstone-Snake-Game/src/snake.cpp
@@ -22,25 +22,24 @@ void Snake::Update() {
 void Snake::UpdateHead() {
   switch (direction) {
     case Direction::kUp:
-      head_y -= speed;
+      head_y = (head_y == 0) ? 0 : head_y - speed;
       break;
 
     case Direction::kDown:
-      head_y += speed;
+      head_y = (head_y == grid_height) ? grid_height : head_y + speed;
       break;
 
     case Direction::kLeft:
-      head_x -= speed;
+      head_x = (head_x == 0) ? 0 : head_x - speed;
       break;
 
     case Direction::kRight:
-      head_x += speed;
+      head_x = (head_x == grid_width) ? grid_width : head_x + speed;
       break;
   }
 
   // Wrap the Snake around to the beginning if going off of the screen.
-  head_x = fmod(head_x + grid_width, grid_width);
-  head_y = fmod(head_y + grid_height, grid_height);
+
 }
 
 void Snake::UpdateBody(SDL_Point &current_head_cell, SDL_Point &prev_head_cell) {
